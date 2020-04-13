73e794c39d05a329de8c311cd4eda2b1613a94b7
shenghone <shenghone@gmail.com>
2020-04-12 20:52:16 -0400

    change env varialbe

--- src/Components/DataWrapper/DataWrapper.jsx
+++ src/Components/DataWrapper/DataWrapper.jsx
@@ -8,10 +8,10 @@ const DataWrapper = ({ children }) => {
   useEffect(() => {
     const getData = async () => {
       const { data: news } = await axios(
-        `https://newsapi.org/v2/everything?q=canada&language=en&pageSize=30&sortBy=publishedAt&apiKey=${process.env.REACT_APP_NEWS_KEY}`
+        `https://newsapi.org/v2/everything?q=canada&language=en&pageSize=30&sortBy=publishedAt&apiKey=c593132c2e4c43799c7767555ad39b31`
       );
       const { data: picData } = await axios(
-        `https://api.unsplash.com/photos/?per_page=30&order_by=latest&client_id=${process.env.REACT_APP_PIC_KEY}`
+        `https://api.unsplash.com/photos/?per_page=30&order_by=latest&client_id=b8fc902b735b64caf1b5bc98cc70eea86b111e904e2f100f80a5023444c41242`
       );
       const n = news.articles.filter((nn) => nn.urlToImage);
       setData({
